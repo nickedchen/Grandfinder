64cc7e17d1365ea1d06cf39743c5a029a5076321
Adrian Ardika Kusuma <ardika7620@gmail.com>
2022-11-26 14:24:33 -0800

    added
    ganti

---
+++ app/src/main/java/com/google/firebase/codelab/grandfinder/model/landingPage.java
@@ -0,0 +1,16 @@
+package com.google.firebase.codelab.grandfinder.model;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.codelab.grandfinder.R;
+
+public class landingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+    }
+}
