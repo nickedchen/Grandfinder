25b18ee2996be5a95fd67f527cd799bb38c48bf3
Adrian Ardika Kusuma <ardika7620@gmail.com>
2022-11-30 22:15:29 -0800

    name changes after like and undo

--- app/src/main/java/com/google/firebase/codelab/grandfinder/landingPage.java
+++ app/src/main/java/com/google/firebase/codelab/grandfinder/landingPage.java
@@ -31,53 +31,70 @@ import com.firebase.ui.auth.AuthUI;
 
 public class landingPage extends AppCompatActivity {
     private DatabaseReference database = FirebaseDatabase.getInstance().getReference();
-
+    int i = 0;
+    ArrayList<String> names = new ArrayList<>();
+    ArrayList<String> hobbies = new ArrayList<>();
+    ArrayList<String> personality = new ArrayList<>();
+    ArrayList<String> habits = new ArrayList<>();
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_landing_page);
-        ArrayList<String> names = new ArrayList<>();
-        ArrayList<String> hobbies = new ArrayList<>();
-        ArrayList<String> personality = new ArrayList<>();
-        ArrayList<String> habits = new ArrayList<>();
-        getData(names, hobbies, personality, habits);
+        next();
         System.out.println("DONE");
         System.out.println(names.toString());
-
     }public void jump(View view){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
-    }public void getData(ArrayList<String> names,ArrayList<String> hobbies,ArrayList<String> personality,ArrayList<String> habits){
+    }public void getData(ArrayList<String> names,ArrayList<String> hobbies,ArrayList<String> personality,ArrayList<String> habits, String bo){
+        System.out.println("Current is " + i);
         TextView txt = findViewById(R.id.nameBio);
         DatabaseReference myRef = database.child("UserInfo");
         System.out.println(myRef.toString());
         DatabaseReference ex = myRef.child("User1");
-
+        if(bo == "undo"){
+            i = i-1;
+        }
+//        names.clear();
+//        hobbies.clear();
+//        personality.clear();
+//        habits.clear();
         myRef.addValueEventListener(new ValueEventListener() {
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                // this method is call to get the realtime
-                TextView txt = findViewById(R.id.nameBio);
-                for(DataSnapshot snapshot1 : snapshot.getChildren()) {
-                    String ma  = snapshot1.child("Name").getValue().toString();
-                    String ma2  = snapshot1.child("Hobbies").getValue().toString();
-                    String ma3  = snapshot1.child("Personality").getValue().toString();
-                    String ma4  = snapshot1.child("Habits").getValue().toString();
-                    txt.setText(ma);
-                    names.add(ma);
-                    hobbies.add(ma2);
-                    personality.add(ma3);
-                    habits.add(ma4);
-
+                try {
+                    // this method is call to get the realtime
+                    TextView txt = findViewById(R.id.nameBio);
+                    for (DataSnapshot snapshot1 : snapshot.getChildren()) {
+                        String ma = snapshot1.child("Name").getValue().toString();
+                        String ma2 = snapshot1.child("Hobbies").getValue().toString();
+                        String ma3 = snapshot1.child("Personality").getValue().toString();
+                        String ma4 = snapshot1.child("Habits").getValue().toString();
+                        /////////
+                        names.add(ma);
+                        hobbies.add(ma2);
+                        personality.add(ma3);
+                        habits.add(ma4);
+                        //////////
+                    }
+                    TextView txt2 = findViewById(R.id.bio1);
+                    String test1 = "Hobbies: " + hobbies.get(i) + "\n";
+                    String test2 = "Personality: " + personality.get(i) + "\n";
+                    String test3 = "Habits: " + habits.get(i) + "\n";
+                    String initial = test1 + test2 + test3;
+                    txt2.setText(initial);
+                    txt.setText(names.get(i).toString());
+                    i++;
+                    if(i >= names.size()-1){
+                        i = names.size()-1;
+                    }else if( i < 0){
+                        i= 0;
+                    }
+                    System.out.println("After is " + i);
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "No more people", Toast.LENGTH_SHORT).show();
                 }
-                TextView txt2 = findViewById(R.id.bio1);
-                String test1 = "Hobbies: " + hobbies.get(0) + "\n";
-                String test2 = "Personality: " + personality.get(0) + "\n";
-                String test3 = "Habits: " + habits.get(0) + "\n";
-                String initial = test1 + test2 + test3;
-                txt2.setText(initial);
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 // calling on cancelled method when we receive
@@ -136,6 +153,10 @@ public class landingPage extends AppCompatActivity {
     private void signOut() {
         AuthUI.getInstance().signOut(this);
         startActivity(new Intent(this, SignInActivity.class));
+    }public void undo(View view){
+        i = i-1;
+        System.out.println(i);
+        getData(names, hobbies, personality, habits, "undo");
     }
 
     public void notif(View view){
@@ -145,6 +166,7 @@ public class landingPage extends AppCompatActivity {
         next();
     }public void next(){
 
+        getData(names, hobbies, personality, habits, "do");
     }
     public void setText(ArrayList<String> names,ArrayList<String> hobbies,ArrayList<String> personality,ArrayList<String> habits){
 
